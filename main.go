a5d6e266a859f41deba7d077ce2200cfacf4331b
ji21 <jeffreyip54@gmail.com>
2022-02-02 17:42:55 +0000

    added ws for signalling

--- main.go
+++ main.go
@@ -1,14 +1,46 @@
 package main
 
 import (
-	"Go-Streaming-Server/server"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/websocket"
 )
 
-func main() {
-	http.HandleFunc("/", server.FetchNewsRequestHandler)
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
 
+func reader(conn *websocket.Conn) {
+	for {
+		messageType, message, err := conn.ReadMessage()
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println(message)
+
+		if err := conn.WriteMessage(messageType, message); err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	reader(ws)
+}
+
+func setRoutes() {
+	http.HandleFunc("/ws", wsHandler)
+}
+
+func main() {
+	setRoutes()
 	log.Println("Starting server on port 8000")
 	if err := http.ListenAndServe(":8000", nil); err != nil {
 		log.Fatal(err)
